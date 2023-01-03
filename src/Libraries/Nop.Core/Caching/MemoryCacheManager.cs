2fbd491dbf414d79e1200342074c80d197898e10
Rickard von Haugwitz <rickardvh@gmail.com>
2023-01-03 15:30:12 +0100

    wip

--- src/Libraries/Nop.Core/Caching/MemoryCacheManager.cs
+++ src/Libraries/Nop.Core/Caching/MemoryCacheManager.cs
@@ -10,10 +10,12 @@ using Nop.Core.Configuration;
 namespace Nop.Core.Caching
 {
     /// <summary>
-    /// Represents a memory cache manager 
+    /// A memory cache manager that locks the acquisition task
     /// </summary>
-    public partial class MemoryCacheManager : CacheKeyService, ILocker, IStaticCacheManager
+    public partial class MemoryCacheManager : CacheKeyService, IStaticCacheManager, ILocker
     {
+        public const string LOCK_PREFIX = "MemoryCacheManager.Lock";
+
         #region Fields
 
         // Flag: Has Dispose already been called?
@@ -36,7 +38,49 @@ namespace Nop.Core.Caching
         #endregion
 
         #region Utilities
-        
+
+        private static string GetLockKey(string key)
+        {
+            return $"{LOCK_PREFIX}-{key}";
+        }
+
+        private async Task<CacheLock> AcquireLockAsync(string key)
+        {
+            while (true)
+            {
+                var cacheLock = _memoryCache.GetOrCreate(GetLockKey(key), _ => new Lazy<CacheLock>(() => new(), true)).Value;
+                try
+                {
+                    await cacheLock.WaitAsync();
+                    return cacheLock;
+                }
+                catch   // cacheLock was removed while waiting, acquire a new instance
+                {
+                }
+            }
+        }
+
+        private async void OnEvictionAsync(object key, object value, EvictionReason reason, object state)
+        {
+            if (reason == EvictionReason.Replaced)
+                return;
+
+            var lockKey = GetLockKey(key as string);
+            if (_memoryCache.TryGetValue(lockKey, out Lazy<CacheLock> lazy))
+            {
+                try
+                {
+                    await lazy.Value.WaitAsync();   // let current tasks finish, then lock
+                    _memoryCache.Remove(lockKey);
+                    lazy.Value.Cancel();
+                    // don't release, rely on cancellation token and let the GC take care of the semaphore
+                }
+                catch   // someone else cancelled first
+                {
+                }
+            }
+        }
+
         /// <summary>
         /// Prepare cache entry options for the passed key
         /// </summary>
@@ -57,32 +101,30 @@ namespace Nop.Core.Caching
                 var tokenSource = _prefixes.GetOrAdd(keyPrefix, new CancellationTokenSource());
                 options.AddExpirationToken(new CancellationChangeToken(tokenSource.Token));
             }
+            options.RegisterPostEvictionCallback(OnEvictionAsync);
 
             return options;
         }
 
-        /// <summary>
-        /// Remove the value with the specified key from the cache
-        /// </summary>
-        /// <param name="cacheKey">Cache key</param>
-        /// <param name="cacheKeyParameters">Parameters to create cache key</param>
-        private void Remove(CacheKey cacheKey, params object[] cacheKeyParameters)
+        private async Task<T> SetAsync<T>(CacheKey key, Func<Task<T>> acquire, bool forceOverwrite)
         {
-            cacheKey = PrepareKey(cacheKey, cacheKeyParameters);
-            _memoryCache.Remove(cacheKey.Key);
-        }
+            if ((key?.CacheTime ?? 0) <= 0)
+                return await acquire();
 
-        /// <summary>
-        /// Add the specified key and object to the cache
-        /// </summary>
-        /// <param name="key">Key of cached item</param>
-        /// <param name="data">Value for caching</param>
-        private void Set(CacheKey key, object data)
-        {
-            if ((key?.CacheTime ?? 0) <= 0 || data == null)
-                return;
-
-            _memoryCache.Set(key.Key, data, PrepareEntryOptions(key));
+            var cacheLock = await AcquireLockAsync(key.Key);
+            try
+            {
+                if (!forceOverwrite && _memoryCache.TryGetValue(key.Key, out T data))
+                    return data;
+                data = await acquire();
+                if (data != null)
+                    _memoryCache.Set(key.Key, data, PrepareEntryOptions(key));
+                return data;
+            }
+            finally
+            {
+                cacheLock.Release();
+            }
         }
 
         #endregion
@@ -95,11 +137,18 @@ namespace Nop.Core.Caching
         /// <param name="cacheKey">Cache key</param>
         /// <param name="cacheKeyParameters">Parameters to create cache key</param>
         /// <returns>A task that represents the asynchronous operation</returns>
-        public Task RemoveAsync(CacheKey cacheKey, params object[] cacheKeyParameters)
+        public async Task RemoveAsync(CacheKey cacheKey, params object[] cacheKeyParameters)
         {
-            Remove(cacheKey, cacheKeyParameters);
-
-            return Task.CompletedTask;
+            var key = PrepareKey(cacheKey, cacheKeyParameters);
+            var cacheLock = await AcquireLockAsync(key.Key);
+            try
+            {
+                _memoryCache.Remove(key.Key);
+            }
+            finally
+            {
+                cacheLock.Release();
+            }
         }
 
         /// <summary>
@@ -119,13 +168,7 @@ namespace Nop.Core.Caching
 
             if (_memoryCache.TryGetValue(key.Key, out T result))
                 return result;
-
-            result = await acquire();
-
-            if(result != null)
-                await SetAsync(key, result);
-
-            return result;
+            return await SetAsync(key, acquire, false);
         }
 
         /// <summary>
@@ -138,23 +181,9 @@ namespace Nop.Core.Caching
         /// A task that represents the asynchronous operation
         /// The task result contains the cached value associated with the specified key
         /// </returns>
-        public async Task<T> GetAsync<T>(CacheKey key, Func<T> acquire)
+        public Task<T> GetAsync<T>(CacheKey key, Func<T> acquire)
         {
-            if ((key?.CacheTime ?? 0) <= 0)
-                return acquire();
-
-            var result = _memoryCache.GetOrCreate(key.Key, entry =>
-            {
-                entry.SetOptions(PrepareEntryOptions(key));
-
-                return acquire();
-            });
-
-            //do not cache null value
-            if (result == null)
-                await RemoveAsync(key);
-
-            return result;
+            return GetAsync(key, () => Task.FromResult(acquire()));
         }
 
         /// <summary>
@@ -166,18 +195,11 @@ namespace Nop.Core.Caching
         /// <returns>The cached value associated with the specified key</returns>
         public T Get<T>(CacheKey key, Func<T> acquire)
         {
-            if ((key?.CacheTime ?? 0) <= 0)
-                return acquire();
-
-            if (_memoryCache.TryGetValue(key.Key, out T result))
-                return result;
-
-            result = acquire();
-
-            if (result != null)
-                Set(key, result);
-
-            return result;
+            var getTask = GetAsync(key, acquire);
+            getTask.Wait();
+            if (getTask.IsCompletedSuccessfully)
+                return getTask.Result;
+            throw getTask.Exception;
         }
 
         /// <summary>
@@ -188,38 +210,9 @@ namespace Nop.Core.Caching
         /// <returns>A task that represents the asynchronous operation</returns>
         public Task SetAsync(CacheKey key, object data)
         {
-            Set(key, data);
-
+            if (data != null)
+                SetAsync(key, () => Task.FromResult(data), true).Wait();
             return Task.CompletedTask;
-        }
-        
-        /// <summary>
-        /// Perform some action with exclusive in-memory lock
-        /// </summary>
-        /// <param name="key">The key we are locking on</param>
-        /// <param name="expirationTime">The time after which the lock will automatically be expired</param>
-        /// <param name="action">Action to be performed with locking</param>
-        /// <returns>True if lock was acquired and action was performed; otherwise false</returns>
-        public bool PerformActionWithLock(string key, TimeSpan expirationTime, Action action)
-        {
-            //ensure that lock is acquired
-            if (_memoryCache.TryGetValue(key, out _))
-                return false;
-
-            try
-            {
-                _memoryCache.Set(key, key, expirationTime);
-
-                //perform action
-                action();
-
-                return true;
-            }
-            finally
-            {
-                //release lock even if action fails
-                _memoryCache.Remove(key);
-            }
         }
 
         /// <summary>
@@ -259,9 +252,6 @@ namespace Nop.Core.Caching
             return Task.CompletedTask;
         }
 
-        /// <summary>
-        /// Dispose cache manager
-        /// </summary>
         public void Dispose()
         {
             Dispose(true);
@@ -274,10 +264,18 @@ namespace Nop.Core.Caching
             if (_disposed)
                 return;
 
-            if (disposing) 
-                _memoryCache.Dispose();
+            if (disposing)
+            {
+                _clearToken.Dispose();
+                // don't dispose of the MemoryCache, as it is injected
+            }
 
             _disposed = true;
+        }
+
+        public bool PerformActionWithLock(string resource, TimeSpan expirationTime, Action action)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
